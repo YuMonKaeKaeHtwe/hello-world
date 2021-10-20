8312a9e198bdd44638ac6c870bb1f06f5b190032
Yu Mon <92815328+YuMonKaeKaeHtwe@users.noreply.github.com>
2021-10-20 22:36:40 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log('Hello World');
+console.error('This is an error');
+// String, Numbers, Boolean, null, undefined
+
+const name = 'Yumon';
+const age = 26;
+const rating = 5;
+
+
+console.log(typeof name)
+console.log(typeof age)
+
+//Concatenation
+console.log('My name is ' +() name + ' and I am ' + age);
+// Template
+
+const fruits = 
